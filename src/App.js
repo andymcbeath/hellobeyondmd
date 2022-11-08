ea1b43b2fe98a923eb60fb00c1714f940604b9c5
Andy McBeath <mcbeath1@gmail.com>
2022-11-08 09:35:45 -0600

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  const CLIENT_ID = "";
+  const REDIRECT_URI = "http://localhost:3000";
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+  const RESPONSE_TYPE = "token";
+
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <h1>Hello BeyondMD!</h1>
+      <a
+        href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+      >
+        Login to Spotify
+      </a>
+    </div>
+  );
+}
+
+export default App;
